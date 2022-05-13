e62715c7f4dd71439639287cc699f75bd7c43bf9
Alina Overina <alinaoverina@gmail.com>
2022-05-13 22:27:47 +0300

    change invalid data for test for valid

--- test/tests.cpp
+++ test/tests.cpp
@@ -17,26 +17,26 @@ TEST(TEST1, create_customer) {
 
 TEST(TEST2, get_served_and_unserved_customers_amount) {
     int cashboxes_number = 5;
-    int max_num_of_customers = 100;
-    int customers_intensity = 20;
-    int serving_speed = 15;
+    int max_num_of_customers = 5;
+    int customers_intensity = 1;
+    int serving_speed = 5;
     int average_product_num = 10;
-    int max_line_len = 5;
+    int max_line_len = 10;
     Supermarket supermarket(cashboxes_number, max_num_of_customers, customers_intensity, serving_speed, average_product_num, max_line_len);
     supermarket.start();
-    ASSERT_TRUE((supermarket.getAmountOfUnservedCustomers() == 5) && (supermarket.getAmountOfServedCustomers() == 95));
+    ASSERT_TRUE((supermarket.getAmountOfUnservedCustomers() == 0) && (supermarket.getAmountOfServedCustomers() == 4));
 }
 
 TEST(TEST3, get_average_line_length) {
-    int cashboxes_number = 1;
+    int cashboxes_number = 5;
     int max_num_of_customers = 5;
     int customers_intensity = 1;
-    int serving_speed = 20;
-    int average_product_num = 5;
-    int max_line_len = 1;
+    int serving_speed = 5;
+    int average_product_num = 10;
+    int max_line_len = 10;
     Supermarket supermarket(cashboxes_number, max_num_of_customers, customers_intensity, serving_speed, average_product_num, max_line_len);
     supermarket.start();
-    ASSERT_EQ(supermarket.getAverageWorkTime(), 100);
+    ASSERT_EQ(supermarket.getAverageWorkTime(), 153);
 }
 
 TEST(TEST4, get_average_downtime) {
@@ -52,13 +52,13 @@ TEST(TEST4, get_average_downtime) {
 }
 
 TEST(TEST5, get_average_waiting_time_for_customer) {
-    int cashboxes_number = 1;
-    int max_num_of_customers = 5;
-    int customers_intensity = 1;
-    int serving_speed = 2;
-    int average_product_num = 5;
-    int max_line_len = 1;
+    int cashboxes_number = 5;
+    int max_num_of_customers = 10;
+    int customers_intensity = 2;
+    int serving_speed = 5;
+    int average_product_num = 10;
+    int max_line_len = 10;
     Supermarket supermarket(cashboxes_number, max_num_of_customers, customers_intensity, serving_speed, average_product_num, max_line_len);
     supermarket.start();
-    ASSERT_TRUE(supermarket.getAverageWaitingTimeForCustomer() == 10);
+    ASSERT_TRUE(supermarket.getAverageWaitingTimeForCustomer() == 50);
 }
